583f31d6fcdff4bc0b64d06c7db397a3189224d3
arthurrroth <127439987+arthurrroth@users.noreply.github.com>
2023-06-22 11:12:00 +0200

    Get Detialed Movie Data

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import SearchSort from '../components/SearchSort';
+import './HomePage.css'
 
 const HomePage = () => {
   return (
-    <div>
-        <Link to={"/search"}>HomePage</Link>
+    <div className='home'>
+        <h1>Welcome</h1>
+        <SearchSort/>
     </div>
   )
 }
