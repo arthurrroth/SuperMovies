d0b94b738191523df519c64ba462c668531f04df
arthurrroth <127439987+arthurrroth@users.noreply.github.com>
2023-06-26 06:28:53 +0200

    CleanUp + SearchPage Rework

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 import HomePage from './pages/HomePage'
-import SearchResultPage from './pages/SearchResultPage'
 import MovieDetailsPage from './pages/MovieDetailsPage'
 import VideoPlayerPage from './pages/VideoPlayerPage'
 import { MovieDataContext, InteractionContext } from './Context'
 import {  DiscoveredMovies, Genres_Movies } from './API/config/required';
 import { fetchedData } from './API/config/required'
+import SearchPage from './components/Search/SearchPage'
+
 
 const App = () => {
   const [discoveredMovies, setDiscoveredMovies] = useState([]);
@@ -15,10 +16,11 @@ const App = () => {
   const [selectedMovie, setSelectedMovie] = useState();
   const [genresMovie, setGeneresMovie] = useState([]);
   const [clickedGenre, setClickedGenre] = useState([]);
-  const [favouritedMovies, setFavMovies] = useState([]);
-  const [downlaodedMovies, setDownloadedMovies] = useState([]);
-
-    const run_Requiered = async () => {
+  const [favouritedMovies, setFavMovies] = useState(detailedMovies);
+  const [downlaodedMovies, setDownloadedMovies] = useState(detailedMovies);
+  const [filteredMovies, setFilteredMovies] = useState(detailedMovies);
+    
+  const run_Requiered = async () => {
 
       const Movies = await DiscoveredMovies;
       const MovieGenres = await Genres_Movies;
@@ -27,53 +29,41 @@ const App = () => {
   
     };
 
-    // Load Detailed Movie Data based on the Discovered Movies fetch
-
     useEffect(() => {
         
         if (discoveredMovies) {
             discoveredMovies.map(async (movie) => {
+              // Fetch Detailed Movie Data
                 const response = await fetchedData(`https://api.themoviedb.org/3/movie/${movie.id}`);
                 setDetailedMovies((prev) => [...prev, response]);
-            
               })
         };
-        
-        
-      console.log({discoveredMovies});
-      
     }, [ discoveredMovies ]);
-
-  useEffect(() => {
-    console.log({detailedMovies});
-    console.log(detailedMovies.length);
-    console.log({genresMovie});
-    
-
-  },[detailedMovies])
     
 
   useEffect(() => {
-
-  run_Requiered();
-
+    run_Requiered();
   }, [])
 
   return (
-    
     <InteractionContext.Provider value={{clickedGenre, setClickedGenre}}>
-        <MovieDataContext.Provider value={{detailedMovies, setDetailedMovies, selectedMovie, setSelectedMovie, genresMovie, favouritedMovies, setFavMovies, downlaodedMovies, setDownloadedMovies}}>
+        <MovieDataContext.Provider value={{detailedMovies, setDetailedMovies,
+                                           selectedMovie, setSelectedMovie,
+                                           genresMovie, favouritedMovies, 
+                                           setFavMovies, downlaodedMovies, 
+                                           setDownloadedMovies, filteredMovies, setFilteredMovies}}>
           <BrowserRouter>
             <Routes>
               <Route path='/landing' element={<LandingPage/>}/>
               <Route path='/' element={<HomePage/>}/>
-              <Route path='/search/:searchParam' element={<SearchResultPage/>}/>
+              <Route path='/search' element={<SearchPage/>}/>
               <Route path='/movie/:id' element={<MovieDetailsPage/>}/>
               <Route path='/movie/:id/video-player' element={<VideoPlayerPage/>}/>
             </Routes>
           </BrowserRouter>
         </MovieDataContext.Provider>
       </InteractionContext.Provider>
+      
  
   )
 }
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,79 @@
+import "./FloatBar.css";
+
+import React from "react";
+import { useState } from "react";
+import { NavLink, useLocation } from "react-router-dom";
+
+import Home from "../../public/home_svg/Home";
+import Download from "../../assets/icons/download_svg/Download";
+import Favorite from "../../public/favorite_svg/Favorite";
+import Profile from "../../public/profile_svg/Profile";
+
+const NavBar = () => {
+  const [active, setActive] = useState("");
+  const location = useLocation();
+
+  const buttonClick = (navText) => {
+    setActive(navText);
+  };
+
+  const isHomePage = location.pathname === "/" || location.pathname === "/Search" || location.pathname.startsWith("/search");
+
+  return (
+    <section className="navbar">
+      <article className="home-container">
+        <NavLink to="/" onClick={() => buttonClick("Home")}>
+        <Home
+            fill={
+              active === "Home" || isHomePage ? "var(--element-color-red)": "#97AABD"
+            }
+          />
+        </NavLink>
+        {(location.pathname === "/" || "isHomePage") && (
+          <NavLink to="/" className="home-text"><p>Home</p></NavLink>)}
+      </article>
+
+      <article className="download-container">
+        <NavLink
+          // to="/download"
+          onClick={() => buttonClick("Downloads")}
+        >
+          <Download
+            fill={
+              active === "Downloads" ? "var(--element-color-red)" : "#97AABD"
+            }
+          />
+        </NavLink>
+        {active === "Downloads" && <p className="dl-text">{active}</p>}
+      </article>
+
+      <article className="favorite-container">
+        <NavLink
+          // to="/favorite"
+          onClick={() => buttonClick("Favorites")}
+        >
+          <Favorite
+            fill={
+              active === "Favorites" ? "var(--element-color-red)" : "#97AABD"
+            }
+          />
+        </NavLink>
+        {active === "Favorites" && <p className="favorite-text">{active}</p>}
+      </article>
+
+      <article className="profile-container">
+        <NavLink
+          // to="/profile"
+          onClick={() => buttonClick("Profile")}
+        >
+          <Profile
+            fill={active === "Profile" ? "var(--element-color-red)" : "#97AABD"}
+          />
+        </NavLink>
+        {active === "Profile" && <p className="profile-text">{active}</p>}
+      </article>
+    </section>
+  );
+};
+
+export default NavBar;
